cbb585c2a408309e5cc36916539ccdc4c5bacef0
DoYam <doniang1123@gmail.com>
2024-02-28 21:30:50 +0900

    [add] map logic in Invest page

--- src/Router.js
+++ src/Router.js
@@ -7,12 +7,14 @@ import InputPrice from "./pages/invest/InputPrice";
 import RatioInvest from "./pages/invest/ratioInvest";
 import LoadingInvest from "./pages/invest/LoadingInvest";
 import Complete from "./pages/invest/Complete";
+import InvestDetail from "./pages/invest/InvestDetail.jsx";
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/invest" element={<Invest />} />
+      <Route path="/invest-detail" element={<InvestDetail />} />
       <Route path="/current-invest" element={<CurrentInvest />} />
       <Route path="/mypage" element={<Mypage />} />
       <Route path="/input-price" element={<InputPrice />} />
