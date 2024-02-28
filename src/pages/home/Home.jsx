1f88547633013e2f3ee177930b4c779a03a645f1
rmfosem613 <rmfosem613@gmail.com>
2024-02-29 02:25:50 +0900

    Home
    디자인 수정 반영

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -19,12 +19,17 @@ function Home(props) {
         <img src={BannerImg} className="bannerImg" alt="Example Image" />
         <AssetBox className="recentAsset">
           <p className="recent">현재</p>
-          <p className="asset">김구름님의 자산</p>
-          <p className="price">100,000원</p>
+          <p className="asset">
+            <span className="nameAsset">김구름</span>님의 자산
+          </p>
+          <p className="price">
+            <span>100,000원</span>
+            {">"}
+          </p>
         </AssetBox>
 
         <p className="firstCate">지금 인기 있는 빈집</p>
-        <p className="firstAll">모두보기{">"}</p>
+        <p className="firstAll">모두보기 {">"}</p>
 
         <CardView>
           <Card>
@@ -84,7 +89,7 @@ function Home(props) {
         </CardView>
 
         <p className="secondCate">이제 나온 신규 빈집</p>
-        <p className="secondAll">모두보기{">"}</p>
+        <p className="secondAll">모두보기 {">"}</p>
 
         <CardView>
           <Card>
