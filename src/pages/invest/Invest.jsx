084c7071a028bac8bc93e74b976be94c5896e319
rmfosem613 <rmfosem613@gmail.com>
2024-02-29 03:04:42 +0900

    invest filter button
    버튼 css 수정

--- src/pages/invest/Invest.jsx
+++ src/pages/invest/Invest.jsx
@@ -1,165 +1,173 @@
-import React, { useEffect, useState } from 'react';
-import NavBar from '../../components/NavBar';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import NavBar from "../../components/NavBar";
+import { useNavigate } from "react-router-dom";
 
 import {
-    TabName,
-    SelectContainer,
-    FilteringContainer,
-    MapButton,
-    FilteringButton,
-    SortingContainer,
-    SortBox,
-    SortBoxLast,
-    HouseContainer,
-    HouseImage,
-    TextContainer,
-    SoonBanner,
-    Title,
-    Price,
-    DateInformation,
-    CurrentContainer,
-    LeftContainer,
-    MoneyLeft,
-    Total,
-    BarGraph,
-    CountContainer,
-    People,
-    DateInfo,
-    IngBanner,
-    DDay,
+  TabName,
+  SelectContainer,
+  FilteringContainer,
+  MapButton,
+  FilteringButton,
+  SortingContainer,
+  SortBox,
+  SortBoxLast,
+  HouseContainer,
+  HouseImage,
+  TextContainer,
+  SoonBanner,
+  Title,
+  Price,
+  DateInformation,
+  CurrentContainer,
+  LeftContainer,
+  MoneyLeft,
+  Total,
+  BarGraph,
+  CountContainer,
+  People,
+  DateInfo,
+  IngBanner,
+  DDay,
 } from "../../styled/invest";
 import MapTest from "./mytest";
 
 function Invest(props) {
-    const [selectedFilter, setSelectedFilter] = useState("전체");
-    const [isMapView, setIsMapView] = useState(false);
-    const [homeList, setHomeList] = useState([]);
-    const navigate = useNavigate();
+  const [selectedFilter, setSelectedFilter] = useState("전체");
+  const [isMapView, setIsMapView] = useState(false);
+  const [homeList, setHomeList] = useState([]);
+  const navigate = useNavigate();
 
-    const handleFilterClick = (filter) => {
-        setSelectedFilter((prevFilter) => (prevFilter === filter ? "" : filter));
+  const handleFilterClick = (filter) => {
+    setSelectedFilter((prevFilter) => (prevFilter === filter ? "" : filter));
+  };
+
+  const toggleView = () => {
+    setIsMapView((prevIsMapView) => !prevIsMapView);
+  };
+  const handleClick = () => {
+    navigate("/invest-detail");
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/api/home/");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setHomeList(data.homeList.reverse());
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
     };
 
-    const toggleView = () => {
-        setIsMapView((prevIsMapView) => !prevIsMapView);
-    };
-    const handleClick = () => {
-        navigate("/invest-detail");
-    };
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('/api/home/');
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                const data = await response.json();
-                setHomeList(data.homeList.reverse());
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
-
-        fetchData();
-    }, []);
-
-    return (
-        <div>
-            <TabName>투자하기</TabName>
-            <SelectContainer>
-                <FilteringContainer>
-                    <FilteringButton
-                        onClick={() => handleFilterClick('전체')}
-                        style={{
-                            backgroundColor: selectedFilter === '전체' ? '#F0F0F5' : '#E8E8EE',
-                            color: selectedFilter === '전체' ? '#000' : '#8F93AE',
-                            border: selectedFilter === '전체' ? '1px solid #000' : '1px solid #ccc'
-                        }}>
-                        전체
-                    </FilteringButton>
-                    <FilteringButton
-                        onClick={() => handleFilterClick('모집중')}
-                        style={{
-                            backgroundColor: selectedFilter === '모집중' ? '#F0F0F5' : '#E8E8EE',
-                            color: selectedFilter === '모집중' ? '#000' : '#8F93AE',
-                            border: selectedFilter === '모집중' ? '1px solid #000' : '1px solid #ccc'
-                        }}>
-                        모집중
-                    </FilteringButton>
-                    <FilteringButton
-                        onClick={() => handleFilterClick('곧 모집')}
-                        style={{
-                            backgroundColor: selectedFilter === '곧 모집' ? '#F0F0F5' : '#E8E8EE',
-                            color: selectedFilter === '곧 모집' ? '#000' : '#8F93AE',
-                            border: selectedFilter === '곧 모집' ? '1px solid #000' : '1px solid #ccc'
-                        }}>
-                        곧 모집
-                    </FilteringButton>
-                </FilteringContainer>
-                <MapButton onClick={toggleView}>{isMapView ? '목록뷰' : '지도보기'}</MapButton>
-            </SelectContainer>
-            <SortingContainer>
-                <SortBox style={{ color: "black" }}>최신 등록</SortBox>
-                <SortBox style={{ color: "gray" }}>오래된 순</SortBox>
-                <SortBox style={{ color: "gray" }}>최저 가격</SortBox>
-                <SortBoxLast style={{ color: "gray" }}>최고 가격</SortBoxLast>
-            </SortingContainer>
-            {
-                isMapView
-                    ?
-                    <MapTest />
-                    :
-                    (
-                        homeList.reverse().map((home, index) => (
-                            <HouseContainer
-                                onClick={handleClick}
-                                key={index}>
-                                <HouseImage style={{
-                                    backgroundImage: `url('data:image/jpeg;base64,${home.before_image_base64}')`,
-                                }}></HouseImage>
-                                <TextContainer>
-                                    {home.is_funding_done === true ?
-                                        <SoonBanner>곧 모집</SoonBanner> :
-                                        (
-                                            <div style={{
-                                                display: 'flex',
-                                                flexDirection: 'row',
-                                                gap: '5px'
-                                            }}>
-                                                <IngBanner>모집 중</IngBanner>
-                                                <DDay>D-{home.funding_last_date}</DDay>
-                                            </div>
-                                        )
-                                    }
-                                    <Title>{home.address}</Title>
-                                    {home.is_funding_done === true ?
-                                        <div>
-                                            <Price>{home.sale_price} (백만원)</Price>
-                                            <DateInformation>2024 7월 중 오픈</DateInformation>
-                                        </div>
-                                        :
-                                        <CurrentContainer>
-                                            <LeftContainer>
-                                                <MoneyLeft>900만 원 남음</MoneyLeft>
-                                                <Total>{home.sale_price} (백만원)</Total>
-                                            </LeftContainer>
-                                            <BarGraph fillPercentage={Number(home.width)}></BarGraph>
-                                            <CountContainer>
-                                                <People>{home.num_of_people}명 참여</People>
-                                                <DateInfo>{home.funding_done_date.split('T')[0]}</DateInfo>
-                                            </CountContainer>
-                                        </CurrentContainer>
-                                    }
-                                </TextContainer>
-                            </HouseContainer>
-                        ))
-                    )
-            }
-            <NavBar />
-        </div >
-    );
+  return (
+    <div>
+      <TabName>투자하기</TabName>
+      <SelectContainer>
+        <FilteringContainer>
+          <FilteringButton
+            onClick={() => handleFilterClick("전체")}
+            style={{
+              backgroundColor:
+                selectedFilter === "전체" ? "#F0F0F5" : "#E8E8EE",
+              color: selectedFilter === "전체" ? "#5F75FA" : "#858899",
+              border: selectedFilter === "전체" ? "1px solid #5F75FA" : "0",
+            }}
+          >
+            전체
+          </FilteringButton>
+          <FilteringButton
+            onClick={() => handleFilterClick("모집중")}
+            style={{
+              backgroundColor:
+                selectedFilter === "모집중" ? "#F0F0F5" : "#E8E8EE",
+              color: selectedFilter === "모집중" ? "#5F75FA" : "#858899",
+              border: selectedFilter === "모집중" ? "1px solid #5F75FA" : "0",
+            }}
+          >
+            모집중
+          </FilteringButton>
+          <FilteringButton
+            onClick={() => handleFilterClick("곧 모집")}
+            style={{
+              backgroundColor:
+                selectedFilter === "곧 모집" ? "#F0F0F5" : "#E8E8EE",
+              color: selectedFilter === "곧 모집" ? "#5F75FA" : "#858899",
+              border: selectedFilter === "곧 모집" ? "1px solid #5F75FA" : "0",
+            }}
+          >
+            곧 모집
+          </FilteringButton>
+        </FilteringContainer>
+        <MapButton
+          className={isMapView ? "listView" : "mapView"}
+          onClick={toggleView}
+        >
+          {isMapView ? "지도보기" : "목록보기"}
+        </MapButton>
+      </SelectContainer>
+      <SortingContainer>
+        <SortBox style={{ color: "black" }}>마감 순</SortBox>
+        <SortBox style={{ color: "gray" }}>오래된 순</SortBox>
+        <SortBox style={{ color: "gray" }}>최저 가격</SortBox>
+        <SortBoxLast style={{ color: "gray" }}>최고 가격</SortBoxLast>
+      </SortingContainer>
+      {isMapView ? (
+        <MapTest />
+      ) : (
+        homeList.reverse().map((home, index) => (
+          <HouseContainer onClick={handleClick} key={index}>
+            <HouseImage
+              style={{
+                backgroundImage: `url('data:image/jpeg;base64,${home.before_image_base64}')`,
+              }}
+            ></HouseImage>
+            <TextContainer>
+              {home.is_funding_done === true ? (
+                <SoonBanner>곧 모집</SoonBanner>
+              ) : (
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    gap: "5px",
+                  }}
+                >
+                  <IngBanner>모집 중</IngBanner>
+                  <DDay>D-{home.funding_last_date}</DDay>
+                </div>
+              )}
+              <Title>{home.address}</Title>
+              {home.is_funding_done === true ? (
+                <div>
+                  <Price>{home.sale_price} (백만원)</Price>
+                  <DateInformation>2024 7월 중 오픈</DateInformation>
+                </div>
+              ) : (
+                <CurrentContainer>
+                  <LeftContainer>
+                    <MoneyLeft>900만 원 남음</MoneyLeft>
+                    <Total>{home.sale_price} (백만원)</Total>
+                  </LeftContainer>
+                  <BarGraph fillPercentage={Number(home.width)}></BarGraph>
+                  <CountContainer>
+                    <People>{home.num_of_people}명 참여</People>
+                    <DateInfo>{home.funding_done_date.split("T")[0]}</DateInfo>
+                  </CountContainer>
+                </CurrentContainer>
+              )}
+            </TextContainer>
+          </HouseContainer>
+        ))
+      )}
+      <NavBar />
+    </div>
+  );
 }
 
 export default Invest;
--- src/styled/invest.js
+++ src/styled/invest.js
@@ -1,181 +1,187 @@
 import styled from "styled-components";
 
 export const TabName = styled.div`
-    font-size: 20px;
-    font-weight: 700;
-    margin: 0 auto;
-    display: flex;
-    justify-content: center;
-    margin-top: 20.5px;
+  font-size: 20px;
+  font-weight: 700;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  margin-top: 20.5px;
 `;
 
 export const SelectContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    padding: 27px 17px;
-    align-items: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 27px 17px;
+  align-items: center;
 `;
 
 export const FilteringButton = styled.button`
-    margin-right: 6px;
-    height: 25px;
-    padding:3px 8px;
-    border-radius: 8px;
+  margin-right: 6px;
+  height: 30px;
+  padding: 3px 8px;
+  border-radius: 8px;
 `;
 
-export const FilteringContainer = styled.div`
-
-`;
+export const FilteringContainer = styled.div``;
 
 export const MapButton = styled.button`
-    color: #1D6CE0;
-    height: 25px;
-    padding:3px 8px;
-    border-radius: 8px;
-    border: solid 2px #1D6CE0;
+  width: 73px; /* 'weight'가 아니라 'width'로 수정 */
+  height: 30px;
+  padding: 3px 8px;
+  border-radius: 8px;
+  color: #767676;
+  border: solid 1px #e8e8ee;
+
+  &.mapView {
+    color: #767676;
+    border: solid 1px #e8e8ee;
+  }
+  &.listView {
+    color: #5f75fa;
+    border: 0;
+    background-color: #d0e0fa;
+  }
 `;
 
 export const SortingContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    padding: 0px 10px;
-    font-size: 14px;
+  display: flex;
+  flex-direction: row;
+  padding: 0px 10px;
+  font-size: 14px;
 `;
 
 export const SortBox = styled.div`
-    padding: 2px 7px;
-    border-right: 1px solid gray;
-    font-size: 14px;
+  padding: 2px 7px;
+  border-right: 1px solid gray;
+  font-size: 14px;
 `;
 
 export const SortBoxLast = styled.div`
-    padding: 2px 7px;
-    font-size: 14px;
+  padding: 2px 7px;
+  font-size: 14px;
 `;
 
 export const HouseContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    padding: 15px 20px;
-    margin: 4px 17px;
-    box-shadow: 4px 4px 8px 0px rgba(67, 0, 209, 0.1);
-    border-radius: 10px;
+  display: flex;
+  flex-direction: row;
+  padding: 15px 20px;
+  margin: 4px 17px;
+  box-shadow: 4px 4px 8px 0px rgba(67, 0, 209, 0.1);
+  border-radius: 10px;
 `;
 
 export const HouseImage = styled.div`
-    width: 100px;
-    height: 100px;
-    border-radius: 10px;
+  width: 100px;
+  height: 100px;
+  border-radius: 10px;
 `;
 
 export const TextContainer = styled.div`
-    margin-left: 24px;
-    width: 175px;
+  margin-left: 24px;
+  width: 175px;
 `;
 
 export const SoonBanner = styled.div`
-    color: #767676;
-    padding: 3px 8px;
-    font-size: 12px;
-    line-height: 24px;
-    background-color: #CDCED6;
-    width: fit-content;
-    border-radius: 8px;
-
+  color: #767676;
+  padding: 3px 8px;
+  font-size: 12px;
+  line-height: 24px;
+  background-color: #cdced6;
+  width: fit-content;
+  border-radius: 8px;
 `;
 
 export const Title = styled.div`
-    line-height: 24px;
-    font-size: 16px;
-    font-weight: 500;
-    margin-top: 2px;
+  line-height: 24px;
+  font-size: 16px;
+  font-weight: 500;
+  margin-top: 2px;
 `;
 
 export const Price = styled.div`
-    line-height: 24px;
-    font-size: 12px;
-    color: #767676;
-    
+  line-height: 24px;
+  font-size: 12px;
+  color: #767676;
 `;
 
 export const DateInformation = styled.div`
-    line-height: 24px;
-    margin-top: 6px;
-    color: #5F75FA;
-    font-weight: 500;
-    font-size: 14px;
+  line-height: 24px;
+  margin-top: 6px;
+  color: #5f75fa;
+  font-weight: 500;
+  font-size: 14px;
 `;
 
 export const CurrentContainer = styled.div`
-    margin-top: 10px;
+  margin-top: 10px;
 `;
 
 export const LeftContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 `;
 
 export const MoneyLeft = styled.div`
-    font-size: 12px;
-    color: #5F75FA;
+  font-size: 12px;
+  color: #5f75fa;
 `;
 
 export const Total = styled.div`
-    font-size: 8px;
-    color: #767676
+  font-size: 8px;
+  color: #767676;
 `;
 export const BarGraph = styled.div`
-    width: 100%;
-    height: 7px; 
-    background-color: #D9D9D9;
-    position: relative;
-    border-radius: 20px;
+  width: 100%;
+  height: 7px;
+  background-color: #d9d9d9;
+  position: relative;
+  border-radius: 20px;
 
-    &::after {
-        content: '';
-        display: block;
-        position: absolute;
-        top: 0;
-        left: 0;
-        height: 100%;
-        background-color: #5F75FA;
-        border-radius: 20px;
-        width: ${props => props.fillPercentage || '0'}%; /* Set the fill percentage */
-    }
+  &::after {
+    content: "";
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    background-color: #5f75fa;
+    border-radius: 20px;
+    width: ${(props) =>
+      props.fillPercentage || "0"}%; /* Set the fill percentage */
+  }
 `;
-export const CountContainer =   styled.div`
-    display: flex;
-    flex-direction: row;
-    color: #858899;
-    font-size: 8px;
-    margin-top: 4px;
-    gap: 5px;
+export const CountContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  color: #858899;
+  font-size: 8px;
+  margin-top: 4px;
+  gap: 5px;
 `;
 export const People = styled.div``;
 export const DateInfo = styled.div``;
 
 export const IngBanner = styled.div`
-    color: #767676;
-    padding: 3px 8px;
-    font-size: 12px;
-    line-height: 24px;
-    background-color: #F0F0F5;
-    width: fit-content;
-    border-radius: 8px;
-    color: #5F75FA;
+  color: #767676;
+  padding: 3px 8px;
+  font-size: 12px;
+  line-height: 24px;
+  background-color: #f0f0f5;
+  width: fit-content;
+  border-radius: 8px;
+  color: #5f75fa;
 `;
 
 export const DDay = styled.div`
-    color: #767676;
-    padding: 3px 8px;
-    font-size: 12px;
-    line-height: 24px;
-    background-color: #5F75FA;
-    width: fit-content;
-    border-radius: 8px;
-    color: white;
+  color: #767676;
+  padding: 3px 8px;
+  font-size: 12px;
+  line-height: 24px;
+  background-color: #5f75fa;
+  width: fit-content;
+  border-radius: 8px;
+  color: white;
 `;
-
-
